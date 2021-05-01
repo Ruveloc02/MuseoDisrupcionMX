cee6b9b8f587dbfbb66f66bd12a2ebd7a8861b67
Carlos Ruiz Velasco Lopez <carlos.ruizvelasco2917@alumnos.udg.mx>
2021-04-30 20:30:21 -0500

    Https en routes

--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\\Http\\Controllers\\InicioController@index')->name('inicio.index');
+Route::get('/', 'App\\Http\\Controllers\\InicioController@index', 'https')->name('inicio.index');
 
-Route::get('/obras', 'App\\Http\\Controllers\\ObraController@index')->name("obras.index");
+Route::get('/obras', 'App\\Http\\Controllers\\ObraController@index', 'https')->name("obras.index");
 
-Route::get('/contactanos', 'App\\Http\\Controllers\\ContactanosController@index')->name('contactanos.index');
-Route::post('/contactanos', 'App\\Http\\Controllers\\ContactanosController@store')->name('contactanos.store');
+Route::get('/contactanos', 'App\\Http\\Controllers\\ContactanosController@index', 'https')->name('contactanos.index');
+Route::post('/contactanos', 'App\\Http\\Controllers\\ContactanosController@store', 'https')->name('contactanos.store');
 
-Route::get('/nosotros', 'App\\Http\\Controllers\\NosotrosController@index')->name('nosotros.index');
+Route::get('/nosotros', 'App\\Http\\Controllers\\NosotrosController@index', 'https')->name('nosotros.index');
 
-Route::get('/recetas', 'App\\Http\\Controllers\\RecetaController@index')->name("recetas.index");
-Route::get('/recetas/create', 'App\\Http\\Controllers\\RecetaController@create')->name("recetas.create");
-Route::post('/recetas', 'App\\Http\\Controllers\\RecetaController@store')->name("recetas.store");
-Route::get('/recetas/{receta}', 'App\\Http\\Controllers\\RecetaController@show')->name("recetas.show"); 
-Route::get('/recetas/{receta}/edit', 'App\\Http\\Controllers\\RecetaController@edit')->name("recetas.edit"); 
-Route::put('/recetas/{receta}', 'App\\Http\\Controllers\\RecetaController@update')->name("recetas.update"); 
-Route::delete('/recetas/{receta}', 'App\\Http\\Controllers\\RecetaController@destroy')->name("recetas.destroy"); 
+Route::get('/recetas', 'App\\Http\\Controllers\\RecetaController@index', 'https')->name("recetas.index");
+Route::get('/recetas/create', 'App\\Http\\Controllers\\RecetaController@create', 'https')->name("recetas.create");
+Route::post('/recetas', 'App\\Http\\Controllers\\RecetaController@store', 'https')->name("recetas.store");
+Route::get('/recetas/{receta}', 'App\\Http\\Controllers\\RecetaController@show', 'https')->name("recetas.show"); 
+Route::get('/recetas/{receta}/edit', 'App\\Http\\Controllers\\RecetaController@edit', 'https')->name("recetas.edit"); 
+Route::put('/recetas/{receta}', 'App\\Http\\Controllers\\RecetaController@update', 'https')->name("recetas.update"); 
+Route::delete('/recetas/{receta}', 'App\\Http\\Controllers\\RecetaController@destroy', 'https')->name("recetas.destroy"); 
 
-Route::get('/blogs', 'App\\Http\\Controllers\\BlogController@index')->name("blogs.index");
-Route::get('/blogs/create', 'App\\Http\\Controllers\\BlogController@create')->name("blogs.create");
-Route::post('/blogs', 'App\\Http\\Controllers\\BlogController@store')->name("blogs.store");
-Route::get('/blogs/{blog}', 'App\\Http\\Controllers\\BlogController@show')->name("blogs.show"); 
-Route::get('/blogs/{blog}/edit', 'App\\Http\\Controllers\\BlogController@edit')->name("blogs.edit"); 
-Route::put('/blogs/{blog}', 'App\\Http\\Controllers\\BlogController@update')->name("blogs.update"); 
-Route::delete('/blogs/{blog}', 'App\\Http\\Controllers\\BlogController@destroy')->name("blogs.destroy"); 
+Route::get('/blogs', 'App\\Http\\Controllers\\BlogController@index', 'https')->name("blogs.index");
+Route::get('/blogs/create', 'App\\Http\\Controllers\\BlogController@create', 'https')->name("blogs.create");
+Route::post('/blogs', 'App\\Http\\Controllers\\BlogController@store', 'https')->name("blogs.store");
+Route::get('/blogs/{blog}', 'App\\Http\\Controllers\\BlogController@show', 'https')->name("blogs.show"); 
+Route::get('/blogs/{blog}/edit', 'App\\Http\\Controllers\\BlogController@edit', 'https')->name("blogs.edit"); 
+Route::put('/blogs/{blog}', 'App\\Http\\Controllers\\BlogController@update', 'https')->name("blogs.update"); 
+Route::delete('/blogs/{blog}', 'App\\Http\\Controllers\\BlogController@destroy', 'https')->name("blogs.destroy"); 
 
 //Route::get('/recetas', 'App\\Http\\Controllers\\RecetaController'); Así se declara con nombre completo del controller
 Auth::routes();
