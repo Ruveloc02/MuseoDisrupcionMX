2f73af3c48b79311b3708a190873ba0d2b0eda9b
Carlos Ruiz Velasco Lopez <carlos.ruizvelasco2917@alumnos.udg.mx>
2021-04-30 19:49:16 -0500

    cambio en cookies y domain dentro de session

--- config/session.php
+++ config/session.php
@@ -126,10 +126,7 @@ return [
     |
     */
 
-    'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
-    ),
+    'cookie' => 'laravel_session',
 
     /*
     |--------------------------------------------------------------------------
@@ -155,7 +152,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => 'https://museodisrupcionmx.com',
 
     /*
     |--------------------------------------------------------------------------
